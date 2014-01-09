565110b7a82ce5df288ce8dcc0ccaba83111ad1c
jiahuixing <jiahuixing@gmail.com>
2014-01-09 21:10:45 +0800

    下载/flashPhone

---
+++ commonLib.py
@@ -0,0 +1,21 @@
+__author__ = 'jiahuixing'
+# -*- coding: utf-8 -*-
+
+import sys
+import time
+
+def debug(msg):
+    print('******%s******' % msg)
+
+
+def getDate():
+    if len(sys.argv) > 1:
+        version = sys.argv[1]
+    else:
+        block = '.'
+        year, mon, day = time.strftime('%Y'), time.strftime('%m'), time.strftime('%d')
+        year = year[-1]
+        mon = str(int(mon))
+        day = str(int(day))
+        version = year + block + mon + block + day
+    return version
