92a9d11cd62b6682acd8e1027025257010d9f9b0
jiahuixing <jiahuixing@gmail.com>
2014-01-10 01:23:04 +0800

    download/commonLib

--- commonLib.py
+++ commonLib.py
@@ -4,6 +4,10 @@ import sys
 import time
 
 
+TYPE_INT = 'int'
+TYPE_STR = 'str'
+
+
 def debug(msg):
     print('******%s******' % msg)
 
@@ -21,4 +25,34 @@ def getDate():
     return version
 
 
+def inputValue(input_length=1, input_type=TYPE_INT):
+    debug('input_type=%s' % input_type)
+    input_info = sys.stdin.read(input_length)
+    if input_type == TYPE_INT:
+        if input_info.isdigit():
+            return input_info
+        else:
+            inputValue(input_length, input_type)
+    elif input_type == TYPE_STR:
+        if input_type in ['']:
+            return input_info
+        else:
+            inputValue(input_length, input_type)
 
+
+def setNumValue(value=0):
+    global num
+    num = value
+
+
+def getNumValue():
+    return num
+
+
+def setStrValue(value=''):
+    global string
+    string = value
+
+
+def getStrValue():
+    return string
