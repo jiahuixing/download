c308bc6a05dc0a2a8e23475a862c9fea89267129
jiahuixing <jiahuixing@gmail.com>
2014-01-10 01:04:01 +0800

    download/commonLib

---
+++ commonLib.py
@@ -0,0 +1,24 @@
+__author__ = 'tracy'
+
+import sys
+import time
+
+
+def debug(msg):
+    print('******%s******' % msg)
+
+
+def getDate():
+    if len(sys.argv) > 1:
+        version = sys.argv[1]
+    else:
+        block = '.'
+        year, mon, day = time.strftime('%Y'), time.strftime('%m'), time.strftime('%d')
+        year = year[-1]
+        mon = str(int(mon))
+        day = str(int(day))
+        version = year + block + mon + block + day
+    return version
+
+
+
