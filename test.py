0b61645f69319afb4402dd4fa8d7c40239f94f5f
jiahuixing <jiahuixing@gmail.com>
2014-01-14 15:24:43 +0800

    test.py

--- test.py
+++ test.py
@@ -1,22 +1,37 @@
 __author__ = 'tracy'
 
+import urllib
+
 from commonLib import *
 from Info import *
 
-xxx = get_time(TIME_YMD)
-yyy = get_time(TIME_HMS)
 
-debug(xxx)
-debug(yyy)
+def test_time():
+    xxx = get_time(TIME_YMD)
+    yyy = get_time(TIME_HMS)
+    debug(xxx)
+    debug(yyy)
 
-index = get_index(X1, CHOOSE)
-print(index)
 
-info = 'Pls choose the num to down the tar:\n'
+def test_index():
+    index = get_index(X1, CHOOSE)
+    print(index)
 
-info = get_info(info, CHOOSE)
-print(info)
 
-file_name = raw_input().strip('\r').strip('\n')
+def test_info():
+    info = 'Pls choose the num to down the tar:\n'
+    info = get_info(info, CHOOSE)
+    print(info)
 
-print(file_name)
+
+def test_read():
+    file_name = raw_input().strip('\r').strip('\n')
+    print(file_name)
+
+
+def test_url():
+    try:
+        web = urllib.urlopen('http://ota.n.miui.com/ota/').code
+        print(web)
+    except IOError, err1:
+        debug(err1)
