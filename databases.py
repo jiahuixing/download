4da509079b80b56b630f5772a17f1117e837b212
jiahuixing <jiahuixing@gmail.com>
2014-01-15 16:34:18 +0800

    databases.py

--- databases.py
+++ databases.py
@@ -1,8 +1,12 @@
 __author__ = 'jiahuixing'
 # -*- coding: utf-8 -*-
 
+import logging
+
 import config as conf
-import MySQLdb, logging, time, _mysql_exceptions, os, hashlib, copy
+import MySQLdb
+import _mysql_exceptions
+
 from commonLib import *
 
 
@@ -59,71 +63,71 @@ class Databases():
 
         return True
 
-	def __reconnect_to_write_db(self):
-		try:
-			self.__conn_w = MySQLdb.connect(host = self.__host_w,
-					port = self.__port_w,
-					user = self.__user_w,
-					passwd = self.__passwd_w,
-					db = self.__db,
-					charset = 'utf8')
-		except Exception, e:
-			logging.error(e)
-			time.sleep(60)
-			self.__reconnect_to_write_db()
+        def __reconnect_to_write_db(self):
+            try:
+                self.__conn_w = MySQLdb.connect(host=self.__host_w,
+                                                port=self.__port_w,
+                                                user=self.__user_w,
+                                                passwd=self.__passwd_w,
+                                                db=self.__db,
+                                                charset='utf8')
+            except Exception, e:
+                logging.error(e)
+                time.sleep(60)
+                self.__reconnect_to_write_db()
 
-	def __reconnect_to_read_db(self):
-		try:
-			self.__conn_r = MySQLdb.connect(host = self.__host_r,
-					port = self.__port_r,
-					user = self.__user_r,
-					passwd = self.__passwd_r,
-					db = self.__db,
-					charset = 'utf8')
-		except Exception, e:
-			logging.error(e)
-			time.sleep(60)
-			self.__reconnect_to_read_db()
+        def __reconnect_to_read_db(self):
+            try:
+                self.__conn_r = MySQLdb.connect(host=self.__host_r,
+                                                port=self.__port_r,
+                                                user=self.__user_r,
+                                                passwd=self.__passwd_r,
+                                                db=self.__db,
+                                                charset='utf8')
+            except Exception, e:
+                logging.error(e)
+                time.sleep(60)
+                self.__reconnect_to_read_db()
 
-	def query(self, sql, args=None):
-		cursor = self.__conn_r.cursor()
-		try:
-			count = cursor.execute(sql, args)
-		except _mysql_exceptions.OperationalError,connect_error:
-			logging.error("Exception %s, sql = %s, args = %s"%(connect_error,sql,args))
-			if connect_error[0] == 2006:
-				self.__reconnect_to_read_db()
-				return self.query(sql, args)
-			elif conf.raise_exception:
-				raise
-			return ()
-		except Exception,e:
-			logging.error("Exception %s , sql = %s, args = %s"%(e,sql,args))
-			if conf.raise_exception:
-				raise
-			return ()
+        def query(self, sql, args=None):
+            cursor = self.__conn_r.cursor()
+            try:
+                count = cursor.execute(sql, args)
+            except _mysql_exceptions.OperationalError, connect_error:
+                logging.error("Exception %s, sql = %s, args = %s" % (connect_error, sql, args))
+                if connect_error[0] == 2006:
+                    self.__reconnect_to_read_db()
+                    return self.query(sql, args)
+                elif conf.raise_exception:
+                    raise
+                return ()
+            except Exception, e:
+                logging.error("Exception %s , sql = %s, args = %s" % (e, sql, args))
+                if conf.raise_exception:
+                    raise
+                return ()
 
-		return cursor.fetchall()
+            return cursor.fetchall()
 
-	def execute(self, sql, args=None):
-		#print sql,args
-		cursor = self.__conn_w.cursor()
-		try:
-			affects = cursor.execute(sql, args)
-		except _mysql_exceptions.OperationalError,connect_error:
-			logging.error("Exception %s , sql = %s , args = %s" % (connect_error, sql, args))
-			if connect_error[0] == 2006:
-				self.__reconnect_to_write_db()
-				self.execute(sql, args)
-			elif conf.raise_exception:
-				raise
-		except Exception,e:
-			logging.error("Exception %s, sql = %s, args = %s"%(e,sql,args))
-			if conf.raise_exception:
-				raise
+        def execute(self, sql, args=None):
+            #print sql,args
+            cursor = self.__conn_w.cursor()
+            try:
+                affects = cursor.execute(sql, args)
+            except _mysql_exceptions.OperationalError, connect_error:
+                logging.error("Exception %s , sql = %s , args = %s" % (connect_error, sql, args))
+                if connect_error[0] == 2006:
+                    self.__reconnect_to_write_db()
+                    self.execute(sql, args)
+                elif conf.raise_exception:
+                    raise
+            except Exception, e:
+                logging.error("Exception %s, sql = %s, args = %s" % (e, sql, args))
+                if conf.raise_exception:
+                    raise
 
-	def my_escape(self, str):
-		"""
+        def my_escape(self, str):
+            """
 		return string
 		"""
-		return self.__conn_w.escape_string(str)
+            return self.__conn_w.escape_string(str)
